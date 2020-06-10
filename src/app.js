ea19033aae1c861988a493a0d739a9cf3caa2f8f
Evans Ekow Prah <ivanspj2@gmail.com>
2020-06-10 11:03:38 +0000

    Init commit

---
+++ src/app.js
@@ -0,0 +1,91 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const location = require('./utils/location')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and viws location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+ app.get('', (req, res) => {
+  res.render('index', {
+        title: 'Weather ',
+        name: 'Ekow'
+  })
+ })
+
+ app.get('/about', (req, res) => {
+   res.render('about', {
+     title: 'About me',
+     quote: 'All my opinions are limited to this feelings: I love my family, I respect my supervisors, and I love my lady.',
+     name: 'Ekow'
+   })
+ })
+
+ app.get('/help', (req, res) => {
+   res.render('help', {
+      title: 'Contact me for support',
+      phone: '+233 508294088',
+      email: 'ivanspj2@gmail.com',
+      name: 'Ekow',
+   })
+ })
+
+app.get('/weather', (req, res) => {
+  if(!req.query.address) {
+      return res.send({
+        error: 'You must provide an address'
+      })
+  }
+
+  location(req.query.address, (error, { latitude, longitude, location } = {}) => {
+      if(error) {
+        return res.send( {error})
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+          if(error) {
+            return res.send({ error })
+          }
+
+          res.send({
+            forecast: forecastData,
+            location,
+            address: req.query.address
+          })
+      })
+  }) 
+})
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Ekow',
+    errorMessage: 'Help article not found'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Ekow',
+    errorMessage: 'Page not found'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const forecast= (latitude, longitude, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=6a2afa7001b5b7a58d4adaf92ffc44c2&query=' + latitude + ',' + longitude + '&units=s'
+
+  request({url, json: true}, (error, { body } ) => {
+    if (error) {
+      callback('Unable to connect to weather service for information!', undefined)
+    } else if (body.error) {
+        callback('Unable to find the location. Provide the correct information.', undefined)
+    } else {
+      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current. humidity + 
+       ' degrees out.' + 'There is ' + body.current.precip + ' chance of rain. ')
+    }
+  })
+}
+
+module.exports = forecast
