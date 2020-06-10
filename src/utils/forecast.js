2b1bc7ac9bb4a5b21abb789af216c59d65ec228f
Evans Ekow Prah <ivanspj2@gmail.com>
2020-06-10 14:31:01 +0000

    Add new data to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast= (latitude, longitude, callback) => {
-  const url = 'http://api.weatherstack.com/current?access_key=6a2afa7001b5b7a58d4adaf92ffc44c2&query=' + latitude + ',' + longitude + '&units=s'
+  const url = 'http://api.weatherstack.com/current?access_key=6a2afa7001b5b7a58d4adaf92ffc44c2&query=' + latitude + ',' + longitude + '&units=f'
 
   request({url, json: true}, (error, { body } ) => {
     if (error) {
@@ -9,8 +9,8 @@ const forecast= (latitude, longitude, callback) => {
     } else if (body.error) {
         callback('Unable to find the location. Provide the correct information.', undefined)
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current. humidity + 
-       ' degrees out.' + 'There is ' + body.current.precip + ' chance of rain. ')
+      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees. ' + 
+         'It feels like ' + body.current.feelslike + ' degrees outside. The humidity is ' + body.current.humidity +   ' %. There is ' + body.current.precip + ' chance of rain. ')
     }
   })
 }
